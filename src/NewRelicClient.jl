07a23f8da2809db86915b7b3ccbce7fb74c3a01f
Patrick Boehnke <patrick.boehnke@relativity.com>
2022-11-23 07:33:47 -0800

    initial template push

---
+++ src/NewRelicClient.jl
@@ -0,0 +1,5 @@
+module NewRelicClient
+
+# Write your package code here.
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using NewRelicClient
+using Test
+
+@testset "NewRelicClient.jl" begin
+    # Write your tests here.
+end
