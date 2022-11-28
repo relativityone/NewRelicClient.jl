214530345475fbdfee52bd22b50cd8c348e6e079
Patrick Boehnke <patrick.boehnke@relativity.com>
2022-11-28 13:31:02 -0800

    parser working

--- src/client.jl
+++ src/client.jl
@@ -16,4 +16,31 @@ mutable struct Client
     end
 end
 
+"""
+    runquery(client::Client, query::String)
 
+This processes the columns and parses them into types that are more reflective of the data
+
+# Argument
+- `client::Client`: This is the NR Client 
+- `query::String`: A valid NRQL query
+"""
+function runquery(client::Client, query::String)
+    querygraqhql = "{
+                       actor {
+                         account(id: $(client.accountid)) {
+                                nrql(query: \"$(query)\") {
+                            results
+                                }
+                            }
+                        }
+                    }"
+    myjson = Dict("query"=>querygraqhql)
+    my_headers = HTTP.mkheaders(["Content-Type" => "application/json" ,"API-Key" => client.apikey])
+    r = HTTP.post(client.newrelicurl,my_headers,JSON.json(myjson))
+    results = String(r.body)
+    resultsjsonobject = stringtojson3array(results)
+    resultsdataframe = arraytodataframe(resultsjsonobject)
+    resultsdataframe = processdatatypes(resultsdataframe)
+    return resultsdataframe
+end
